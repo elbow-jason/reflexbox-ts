91c3580214d0981af84295015b10af19adb1d73e
Jason Goldberger <jason.goldberger@dockyard.com>
2018-06-05 23:05:50 -0700

    added boxprops

--- src/flex.ts
+++ src/flex.ts
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { Box } from './box'
+import { Box, BoxProps } from './box'
 import contextTypes, { ContextTypesI } from './context-types'
 
-export function Flex<P={}>(props: P): React.StatelessComponent<P> {
+export function Flex<P={}>(props: P): React.StatelessComponent<P & BoxProps> {
   return Box<P>(Object.assign({}, props, { flex: true }))
 }
